0200a5c48db01871d07eb40acf26022668d10d06
Maury <maury.junior@ufla.br>
2022-08-23 17:33:47 -0300

    Implementação step com ganho e estado inicial dif 0

--- Pratica_9.m
+++ Pratica_9.m
@@ -25,14 +25,14 @@ t_d_Nish = theta_tau-tau_1_Nish;
 den_Nish = [tau_1_Nish 1];
 H_Nish = tf(num,den_Nish);
 H_Nish = H_Nish*exp(-t_d_Nish*s);
-step(H_Nish)
+% step(H_Nish)
 %H_Nish = tf(num,den_Nish,'InputDelay',t_d_Nish)
 
 %b - Metodo de uma constante de tempo (63,2%)
 tau_1_1const_temp = 15.2-t_d_approx;
 t_d_1const_temp = t_d_approx;
 den_1const = [tau_1_1const_temp 1];
-H_1const = tf(num,den_1const);%*exp(-t_d_1const_temp*s)
+H_1const = tf(num,den_1const)*exp(-t_d_1const_temp*s);
 
 %c - Metodo de quatro constantes de tempo (98%)
 y_4t_approx = 2+0.98*(8-2);
@@ -40,30 +40,30 @@ tau_4_approx = 40 - t_d_approx;
 tau_1_4const_temp = tau_4_approx/4;
 t_d_4const_temp = t_d_approx;
 den_4const = [tau_1_4const_temp 1];
-H_4const = tf(num,den_4const);
+H_4const = tf(num,den_4const)*exp(-t_d_4const_temp*s);
 
 %d - Metodo de Ziegler-Nichols;
 t_3_ZN = 18.3;
 tau_1_ZN = t_3_ZN-t_d_approx;
 t_d_ZN = t_d_approx;
 den_ZN = [tau_1_ZN 1];
-H_ZN = tf(num, den_ZN);
+H_ZN = tf(num, den_ZN)*exp(-t_d_ZN*s);
 
 %e - Metodo de Smith;
 y_283_approx = 2 + 0.283*(8-2);
 t_1_Sm = 7.8;
 t_2_Sm = 15.2;
 tau_1_Sm = 1.5*(t_2_Sm-t_1_Sm);
-t_d_Sm = t_2_Sm-tau_1_Sm
-den_Sm = [tau_1_Sm 1]
-H_Sm = tf(num, den_Sm);
+t_d_Sm = t_2_Sm-tau_1_Sm;
+den_Sm = [tau_1_Sm 1];
+H_Sm = tf(num, den_Sm)*exp(-t_d_Sm*s);
 
 %f - Metodo de Haglund
 t_2_Hag = 10;
-tau_1_ZN = t_2_Hag-t_d_approx;
+tau_1_Hag = t_2_Hag-t_d_approx;
 t_d_Hag = t_d_approx;
-den_Hag = [tau_1_Hag 1]
-H_Hag = tf(num, den_Hag);
+den_Hag = [tau_1_Hag 1];
+H_Hag = tf(num, den_Hag)*exp(-t_d_Hag*s);
 
 %g - Metodo de Sundaresan e Krishnaswami;
 y_353_approx = 2 + 0.353*(8-2);
@@ -72,21 +72,38 @@ t_1_Sun = 8.9;
 t_2_Sun = 23.3;
 tau_1_Sun = 0.67*(t_2_Sun-t_1_Sun);
 t_d_Sun = 1.3*t_1_Sun-0.29*t_2_Sun;
-den_Sun = [tau_1_Sun 1]
-H_Sun = tf(num, den_Sun);
+den_Sun = [tau_1_Sun 1];
+H_Sun = tf(num, den_Sun)*exp(-t_d_Sun*s);
 
 %h - Metodo da inclinacao inicial.
 tau_1_inc_inicial = 17 - t_d_approx;
 t_d_inc_inicial = t_d_approx;
-den_inc = [tau_1_inc_inicial 1]
-H_inc = tf(num, den_inc);
+den_inc = [tau_1_inc_inicial 1];
+H_inc = tf(num, den_inc)*exp(-t_d_inc_inicial*s);
 
 %
-tau_1_approx = [tau_1_Nish,tau_1_1const_temp,tau_1_4const_temp,tau_1_ZN,tau_1_Sm,tau_1_ZN,tau_1_Sun,tau_1_inc_inicial]
-t_d_approx_all = [t_d_Nish,t_d_1const_temp,t_d_4const_temp,t_d_ZN,t_d_Sm,t_d_ZN,t_d_Sun,t_d_inc_inicial]
+tau_1_approx = [tau_1_Nish,tau_1_1const_temp,tau_1_4const_temp,tau_1_ZN,tau_1_Sm,tau_1_Hag,tau_1_Sun,tau_1_inc_inicial]'
+t_d_approx_all = [t_d_Nish,t_d_1const_temp,t_d_4const_temp,t_d_ZN,t_d_Sm,t_d_Hag,t_d_Sun,t_d_inc_inicial]'
+
+T_Nish = 2+lsim(H_Nish,v,t,2);
+T_1const = 2+lsim(H_1const,v,t,2);
+T_4const = 2+lsim(H_4const,v,t,2);
+T_ZN = 2+lsim(H_ZN,v,t,2);
+T_Sm = 2+lsim(H_Sm,v,t,2);
+T_Hag = 2+lsim(H_Hag,v,t,2);
+T_Sun = 2+lsim(H_Sun,v,t,2);
+T_inc = 2+lsim(H_inc,v,t,2);
 
 figure('color',[1 1 1])
-step(H_Nish)
-hold on
+plot(t,T)
+hold all
 grid on
-
+plot(t,T_Nish)
+plot(t,T_1const)
+plot(t,T_4const)
+plot(t,T_ZN)
+plot(t,T_Sm)
+plot(t,T_Hag)
+plot(t,T_Sun)
+plot(t,T_inc)
+legend('Nish','1 const.','4 const.','Ziegler-Nichols','Smith','Haglund','Sundaresan','Inc')
