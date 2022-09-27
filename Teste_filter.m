5ded9ad3276497e8f4f1e36fff4ba46f48797524
maury.junior <maury.junior@ufla.br>
2022-09-27 16:47:28 -0300

    Teste filtragem prat 10

---
+++ Teste_filter.m
@@ -0,0 +1,18 @@
+%% Parte 3 - Resposta ao impulso e resposta em frequencia
+dadosordem2rand = load('dadosordem2aleatorio.txt');
+t_rand = dadosordem2rand(:,1);
+v_rand = dadosordem2rand(:,2);
+T_rand = dadosordem2rand(:,3);
+
+% Identificacao pelo metodo da convolucao
+plot(t_rand,T_rand)
+grid on
+hold on
+
+[ruy lagsuy]=xcorr(v_rand,T_rand);
+[ry lagsy]=xcorr(T_rand);
+
+figure('color',[1 1 1])
+plot(lagsyt,ruy)
+figure('color',[1 1 1])
+plot(lagsy,ry)
